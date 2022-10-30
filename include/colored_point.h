bc9d0844a3b5610a2637c6fd42ef050cf36079df
Martins Zeltins <martinszeltins@gmail.com>
2022-10-30 10:29:23 +0200

    Added comments to explain inheritence

--- include/colored_point.h
+++ include/colored_point.h
@@ -6,6 +6,25 @@
 #include "include/point.h"
 
 struct ColoredPoint {
+    /**
+     * You can implement inheritance by literally embedding the superclass
+     * (Point) as the first member of the subclass (ColoredPoint). Such
+     * nesting of structures always aligns the first data member ‘super’
+     * at the beginning of every instance of the derived structure. This
+     * alignment is guaranteed by the C standard. Specifically, WG14/N1124
+     * Section 6.7.2.1.13 says: “A pointer to a structure object, suitably
+     * converted, points to its initial member. There may be unnamed padding
+     * within a structure object, but not at its beginning”. This alignment
+     * lets you treat a pointer to the derived ColoredPoint struct as a pointer
+     * to the Point base struct. All this is legal, portable, and blessed by
+     * the Standard.
+     * 
+     * With this arrangement, you can always safely pass a pointer to
+     * ColoredPoint to any C function that expects a pointer to Point.
+     * Consequently, all functions designed for the Point structure are
+     * automatically available to the ColoredPoint structure.
+     * They are all inherited.
+     */
     struct Point parent;
     int color;
 };
